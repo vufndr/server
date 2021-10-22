a1ea5129d3b927d71f42949d09246564e316be3e
Richard McDaniel <richard@Richards-Personal-MacBook-Pro.local>
2021-10-21 20:38:05 -0500

    Log metadata

--- app/Jobs/GetDropboxImageMetadata.php
+++ app/Jobs/GetDropboxImageMetadata.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
-use Intervention\Image\Image;
+use Intervention\Image\Facades\Image;
 use Throwable;
 use Spatie\TemporaryDirectory\TemporaryDirectory;
 
