cc8f0ea69720da2809df96ef12dd47ee40af59c4
Richard McDaniel <richard@Richards-Personal-MacBook-Pro.local>
2021-10-21 19:56:04 -0500

    Get metadata

---
+++ app/Jobs/GetDropboxImageMetadata.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use App\Services\DropboxService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Intervention\Image\Image;
+use Throwable;
+
+class GetDropboxImageMetadata implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $image;
+
+    public function __construct(ImageFile $image)
+    {
+        $this->image = $image;
+    }
+
+    public function handle()
+    {
+        $file = app(DropboxService::class)->download($this->user->id, $this->image->path);
+        Log::info($file);
+    }
+}
