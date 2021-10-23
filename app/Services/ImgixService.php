970329a48757c11b1d0f61e8c1c31651f5425889
Richard McDaniel <richard@Richards-Personal-MacBook-Pro.local>
2021-10-23 12:02:16 -0500

    Fix links

--- app/Services/ImgixService.php
+++ app/Services/ImgixService.php
@@ -8,7 +8,7 @@ class ImgixService
 {
     public static function getThumbnailUrl(ImageFile $image)
     {
-        $url = config('services.imgix.url') . '/' . $image->uuid . '/?w=256&h=256';
+        $url = config('services.imgix.url') . '/' . $image->uuid . '?w=256&h=256';
         $signature = md5(config('services.imgix.token') . $url);
         $url .= '&s=' . $signature;
         return $url;
@@ -16,7 +16,7 @@ class ImgixService
 
     public static function getPreviewUrl(ImageFile $image)
     {
-        $url = config('services.imgix.url') . '/' . $image->uuid . '/?w=1024&h=1024';
+        $url = config('services.imgix.url') . '/' . $image->uuid . '?w=1024&h=1024';
         $signature = md5(config('services.imgix.token') . $url);
         $url .= '&s=' . $signature;
         return $url;
