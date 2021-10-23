df17d3de49759fe89774dce05335a11976691307
Richard McDaniel <richard@Richards-Personal-MacBook-Pro.local>
2021-10-23 12:18:27 -0500

    Fix urls

--- app/Services/ImgixService.php
+++ app/Services/ImgixService.php
@@ -3,22 +3,37 @@
 namespace App\Services;
 
 use App\Models\ImageFile;
+use Imgix\UrlBuilder;
 
 class ImgixService
 {
+    public static function getBuilder()
+    {
+        $builder = new UrlBuilder(config('services.imgix.url'));
+        $builder->setSignKey(config('services.imgix.token'));
+        return $builder;
+    }
+
+    public static function getUrl($params)
+    {
+        $params['expires'] = now()->addMinutes(15)->timestamp;
+
+        return static::getBuilder()->createURL($image->uuid, $params);
+    }
+
     public static function getThumbnailUrl(ImageFile $image)
     {
-        $url = config('services.imgix.url') . '/' . $image->uuid . '?w=256&h=256';
-        $signature = md5(config('services.imgix.token') . $url);
-        $url .= '&s=' . $signature;
-        return $url;
+        return static::getUrl($image, [
+            'w' => 256,
+            'h' => 256,
+        ]);
     }
 
     public static function getPreviewUrl(ImageFile $image)
     {
-        $url = config('services.imgix.url') . '/' . $image->uuid . '?w=1024&h=1024';
-        $signature = md5(config('services.imgix.token') . $url);
-        $url .= '&s=' . $signature;
-        return $url;
+        return static::getUrl($image, [
+            'w' => 1024,
+            'h' => 1024,
+        ]);
     }
 }
