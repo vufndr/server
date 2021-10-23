b8a083d209db90dfea7288bf8572a01e8ffeb3c8
Richard McDaniel <richard@Richards-Personal-MacBook-Pro.local>
2021-10-23 11:36:11 -0500

    Forgot how to cat strings

--- app/Services/ImgixService.php
+++ app/Services/ImgixService.php
@@ -9,7 +9,7 @@ class ImgixService
     public static function getThumbnailUrl(ImageFile $image)
     {
         $url = config('services.imgix.url') . '/' . $image->uuid . '/?w=256&h=256';
-        $signature = md5(config('services.imgix.token') + $url);
+        $signature = md5(config('services.imgix.token') . $url);
         $url .= '&s=' . $signature;
         return $url;
     }
@@ -17,7 +17,7 @@ class ImgixService
     public static function getPreviewUrl(ImageFile $image)
     {
         $url = config('services.imgix.url') . '/' . $image->uuid . '/?w=1024&h=1024';
-        $signature = md5(config('services.imgix.token') + $url);
+        $signature = md5(config('services.imgix.token') . $url);
         $url .= '&s=' . $signature;
         return $url;
     }
