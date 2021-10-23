e474542f6da2b0f4c87c2e101cf4b92dd4a61863
Richard McDaniel <richard@Richards-Personal-MacBook-Pro.local>
2021-10-23 13:30:07 -0500

    Facet search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,11 +10,21 @@ class SearchController extends Controller
     {
         request()->validate([
             'query' => 'nullable|string|max:255',
+            'resolution' => 'nullable|string|max:255',
         ]);
+
+        $facetFilters = [];
+
+        if (request()->has('resolution')) {
+            $facetFilters['resolution'] = request('resolution');
+        }
 
         return ImageFile::search(request('query', ''))
             ->where('user_id', auth()->user()->id)
-            ->with(['facets' => ['*']])
+            ->with([
+                'facets' => ['*'],
+                'facetFilters' => $facetFilters,
+            ])
             ->paginate();
     }
 }
